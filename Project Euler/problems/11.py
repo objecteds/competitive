84ffc5ff371acb102414ade07a6d6d493d474013
weiyi <ma.weiyi@dhs.sg>
2021-11-22 11:14:20 +0800

    project euler: added comments

--- Project Euler/problems/11.py
+++ Project Euler/problems/11.py
@@ -1,3 +1,13 @@
+'''
+In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+
+*insert grid here*
+
+The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+
+What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+'''
+
 grid = [
     ("08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08").split(),
     ("49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00").split(),
--- Project Euler/problems/16.py
+++ Project Euler/problems/16.py
@@ -1,3 +1,9 @@
+'''
+2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^(1000)?
+'''
+
 n = 0
 
 for _ in list(map(int, list(str(2**1000)))):
