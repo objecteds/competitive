acfce948ff9cfb81c3ecf7842960bf1ad7fdcae3
weiyi <ma.weiyi@dhs.sg>
2021-11-24 14:15:02 +0800

    hackerrank: c++, solved c-tutorial-pointer

---
+++ HackerRank/C++/c-tutorial-pointer.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+void update(int *a,int *b) {
+    int x = *a;
+    int y = *b;
+    
+    *a = x + y;
+    *b = abs(x - y);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
