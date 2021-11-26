85a9b85b39037e33708e52c5f835a9c5ddb6c668
weiyi <ma.weiyi@dhs.sg>
2021-11-26 14:24:35 +0800

    mrJudge: solved checkmultiplies

---
+++ mrJudge/problems/checkmultiplies.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n % m == 0) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+}
