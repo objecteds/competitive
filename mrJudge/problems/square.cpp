3b05f574320cfe0f6e156b04a68fa05c3125ba8a
weiyi <ma.weiyi@dhs.sg>
2021-11-26 11:53:26 +0800

    mrJudge: solved square

---
+++ mrJudge/problems/square.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n+1; i++) {
+        for (int j = 1; j < n+1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
