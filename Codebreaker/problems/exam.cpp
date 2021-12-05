76290adb28152a3133d1bc3a2960a8a5af306020
weiyi <ma.weiyi@dhs.sg>
2021-12-05 13:08:41 +0800

    Codebreaker: solved exam

---
+++ Codebreaker/problems/exam.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int64_t x, a, b;
+    cin >> x;
+
+    if (x == 1) {
+        cout << "Hello World";
+    } else {
+        cin >> a >> b;
+        cout << a * b;
+    }
+}
