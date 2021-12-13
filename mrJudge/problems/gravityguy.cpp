2641b321f3941dde085e196d86c1df5152bed6e2
weiyi <ma.weiyi@dhs.sg>
2021-12-14 02:05:02 +0800

    mrJudge: solved gravityguy

---
+++ mrJudge/problems/gravityguy.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    vector<int> v;
+
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+
+    sort(v.rbegin(), v.rend());
+
+    for (auto x: v) {
+        cout << x << '\n';
+    }
+}
