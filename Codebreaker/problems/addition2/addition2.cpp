484f3bd82cdfc8617b7f94d2253a74a82b25d6aa
weiyi <ma.weiyi@dhs.sg>
2022-01-01 02:36:19 +0800

    Codebreaker: solved addition2

---
+++ Codebreaker/problems/addition2/addition2.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+int main(){
+    int a, b; std::cin >> a >> b; std::cout << a+b;
+}
+
+// you can move your mouse and click on buttons without triggering the alert by going into inspect element (on firefox at least)
