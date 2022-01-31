db94c69f5b41d12dfc403fa872f09fda556faa8c
weiyi <ma.weiyi@dhs.sg>
2022-02-01 01:57:50 +0800

    PCTC: solved 2021 R0 Q6

---
+++ PCTC/2021 R0/Q6.py
@@ -0,0 +1,11 @@
+q, c = int(input()), input()
+if c == "dobbles":
+	print(q // 2)
+elif c == "dabbles":
+	print(q // 3)
+else:
+	s = input()
+	if s == "dobbles":
+		print(q * 2)
+	else:
+		print(q * 3)
