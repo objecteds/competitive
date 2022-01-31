0b4bfbf6937703a768c8ad1b681f995a552a9087
weiyi <ma.weiyi@dhs.sg>
2022-02-01 01:30:49 +0800

    PCTC: solved 2022 R0 Q7

---
+++ PCTC/PCTC 2022 R0/Q7.py
@@ -0,0 +1,9 @@
+t = input()
+if t == "beetroot":
+    print(70 + 5*int(input()))
+else:
+    k = input()
+    if k == "y":
+        print(50+15+5*int(input()))
+    else:
+        print(50+5*int(input()))
