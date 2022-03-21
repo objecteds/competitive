8bc002aaa3773dada6878fdbe25984b71e4ad401
weiyi <ma.weiyi@dhs.sg>
2022-03-21 23:05:22 +0800

    CodeChef: solved CREDCOINS

---
+++ CodeChef/problems/CREDCOINS/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        int x, y; cin >> x >> y;
+        cout << (x*y) / 100 << '\n';
+    }
+}
