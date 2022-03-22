77139b57e216956dce9075284905dca4e20aed6e
weiyi <ma.weiyi@dhs.sg>
2022-03-22 20:38:24 +0800

    CodeChef: solved NOTEBOOK

---
+++ CodeChef/problems/NOTEBOOK/main.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    int n; cin >> n;
+    cout << n*10 << '\n';
+}
+
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
