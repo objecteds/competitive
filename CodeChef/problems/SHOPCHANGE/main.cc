4c38e22c1fef6fd2465fe343e00c05c4da3cf8ac
weiyi <ma.weiyi@dhs.sg>
2022-03-22 20:29:30 +0800

    CodeChef: solved SHOPCHANGE

---
+++ CodeChef/problems/SHOPCHANGE/main.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    int x; cin >> x;
+    cout << 100-x << '\n';
+}
+
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
