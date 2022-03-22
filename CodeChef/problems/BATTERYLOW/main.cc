bbb3058015e257866fcc13a2c48d2beed4bb2ace
weiyi <ma.weiyi@dhs.sg>
2022-03-22 20:25:12 +0800

    CodeChef: solved BATTERYLOW

---
+++ CodeChef/problems/BATTERYLOW/main.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    int x; cin >> x;
+    x <= 15 ? cout << "Yes\n" : cout << "No\n";
+}
+
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
