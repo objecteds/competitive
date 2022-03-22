76ec99d73fc34420de44876eeac3d7172b694d7a
weiyi <ma.weiyi@dhs.sg>
2022-03-22 20:31:10 +0800

    CodeChef: sovled JCOINS

---
+++ CodeChef/problems/JCOINS/main.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    int x, y ; cin >> x >> y;
+    cout << x*10+y*5 << '\n';
+}
+
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
