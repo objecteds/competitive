d27124f8c63a8e97726f91782a6203ea7016bf14
weiyi <ma.weiyi@dhs.sg>
2022-03-22 20:28:15 +0800

    CodeChef: solved JASSIGNMENTS

---
+++ CodeChef/problems/JASSIGNMENTS/main.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    int x; cin >> x;
+    x + 3 > 10 ? cout << "No\n" : cout << "Yes\n";
+}
+
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
