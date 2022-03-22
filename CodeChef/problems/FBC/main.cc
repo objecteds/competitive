fc783005bcf785a7428dd74079bff70b993ba358
weiyi <ma.weiyi@dhs.sg>
2022-03-22 20:17:47 +0800

    CodeChef: solved FBC

---
+++ CodeChef/problems/FBC/main.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    int k, x; cin >> k >> x;
+    cout << k-x << '\n';
+}
+
+signed main() {
+    int tc; cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
