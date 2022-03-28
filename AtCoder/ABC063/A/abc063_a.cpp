14963cf4d4a9277a20c21d3f09ef22f12e5d1d62
weiyi <ma.weiyi@dhs.sg>
2022-03-28 09:53:06 +0800

    AtCoder: solved abc063_a

---
+++ AtCoder/ABC063/A/abc063_a.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+signed main() {
+    int a, b; cin >> a >> b;
+    a + b < 10 ? cout << a + b : cout << "error";
+}
