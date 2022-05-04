9e761020e887d8980a39a64eeaeaa39af0ecd777
weiyi <ma.weiyi@dhs.sg>
2022-05-04 14:57:16 +0800

    AtCoder: solved abc084_a

---
+++ AtCoder/ABC084/A/abc084_a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+signed main() {
+    int m; cin >> m;
+    cout << 24 + (24 - m);
+}
